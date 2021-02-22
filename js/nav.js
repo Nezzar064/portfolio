aeb0772b1b5c6f1514d51d9bc0205c393f497647
Mathias <math8802>
2021-02-22 21:37:22 +0100

    commit

---
+++ js/nav.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$('.main-menu ul li a').click(function(event) {
+		$('.main-menu ul li a').removeClass('active');
+		$(this).addClass('active');
+		thisAttrHref = $(this).attr('href');
+		thisAttrHrefOffset = $(thisAttrHref).offset().top + 50;
+		$('html,body').animate({scrollTop:thisAttrHrefOffset});
+		event.preventDefault();
+	});
+});
+
+$(window).scroll(function() {
+	$('.container-row').each(function() {
+		containerRowTop = $(this).offset().top;
+		if($(document).scrollTop() > containerRowTop){
+			thisOfs = $(this).attr('id');
+			$('.container-row').removeClass('active');
+			ActiveId = $(this).addClass('active').attr('id');;
+		}
+	});
+	$('.main-menu ul li').each(function(index, el) {
+		thisChildren = $(this).children('a');
+		thisChildrenHref = $(this).children('a').attr('href');
+		if(thisChildrenHref === '#'+ActiveId){
+			$('.main-menu ul li a').removeClass('active');
+			$(thisChildren).addClass('active');
+		}
+	});
+});
+
